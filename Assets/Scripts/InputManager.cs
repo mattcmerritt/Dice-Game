bb73495e38b7a67f1044d5b93643fc32e3408c31
mattcmerritt <mattcmerritt@gmail.com>
2022-07-15 16:58:17 -0400

    smoothed out cursor movement

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,70 +10,65 @@ public class InputManager : MonoBehaviour
     // cursor data
     private int CursorX, CursorY;
     [SerializeField] private GameObject Cursor;
-    private float MoveElapsedTime;
-
-    // UNUSED: arrows for movement
-    // [SerializeField] private GameObject Right, Left, Up, Down;   // arrows
-    // [SerializeField] private GameObject RL, UD, UR, UL, DR, DL;  // connections
-
-    // UNUSED: movement tracking
-    // private char PrevDirection, PrevPrevDirection;
-    // private List<GameObject> MovementArrows;
-    // private List<char> Moves;
+    private Coroutine MovementRoutine;
+    [SerializeField, Range(0f, 0.5f)] private float MoveDuration;
 
     private void Update()
     {
-        /*
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            Debug.Log("Moving Right");
-            CursorX++;
-        } 
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            Debug.Log("Moving Left");
-            CursorX--;
-        }
-        else if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            Debug.Log("Moving Up");
-            CursorY++;
-        }
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            Debug.Log("Moving Down");
-            CursorY--;
-        }
-        */
-
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Moving Right");
             CursorX++;
+
+            if (MovementRoutine != null)
+            {
+                StopCoroutine(MovementRoutine);
+            }
+            MovementRoutine = StartCoroutine(Lerp(Cursor.transform.position, Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z, MoveDuration));
         }
-        else if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Moving Left");
             CursorX--;
+
+            if (MovementRoutine != null)
+            {
+                StopCoroutine(MovementRoutine);
+            }
+            MovementRoutine = StartCoroutine(Lerp(Cursor.transform.position, Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z, MoveDuration));
         }
-        else if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("Moving Up");
             CursorY++;
+
+            if (MovementRoutine != null)
+            {
+                StopCoroutine(MovementRoutine);
+            }
+            MovementRoutine = StartCoroutine(Lerp(Cursor.transform.position, Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z, MoveDuration));
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("Moving Down");
             CursorY--;
+
+            if (MovementRoutine != null)
+            {
+                StopCoroutine(MovementRoutine);
+            }
+            MovementRoutine = StartCoroutine(Lerp(Cursor.transform.position, Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z, MoveDuration));
         }
+    }
 
-        Cursor.transform.position = Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z;
-
-        // smoothing cursor movement
-        /*
-        Vector3 targetPosition = Vector3.right * CursorX + Vector3.up * CursorY + Vector3.forward * Cursor.transform.position.z;
-        Vector3.Lerp(Cursor.transform.position, targetPosition, MoveElapsedTime);
-        MoveElapsedTime += Time.deltaTime;
-        */
+    IEnumerator Lerp(Vector3 position, Vector3 target, float duration)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            Cursor.transform.position = Vector3.Lerp(position, target, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
