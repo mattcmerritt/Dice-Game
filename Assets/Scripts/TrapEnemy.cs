0ed17aa20771e9200ad4ec4dab330d4b8ebbb206
mikepmerritt <mikepmerritt@gmail.com>
2022-07-17 04:56:23 -0400

    made the trap enemy activate traps and move

--- Assets/Scripts/TrapEnemy.cs
+++ Assets/Scripts/TrapEnemy.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class TrapEnemy : Enemy, IUnstackable
 {
-    public Vector2Int ChosenTarget; // button to press
+    [System.Serializable]
+    public class CoordinateArray
+    {
+        public Vector2Int[] coords;
+    }
+
+    public int ChosenIndex; // button to press
     public Vector2Int[] Targets; // the buttons to move to
-    public Vector2Int[][] AreasOfInterest; // the tiles that the enemy will watch for each button
+    public CoordinateArray[] AreasOfInterest; // the tiles that the enemy will watch for each button
+    public Trap[] Traps; // the traps for each button when pressed
+
+    // animator
+    [SerializeField] private Animator Ani;
+    [SerializeField] private string AnimationClipPrefix;
+
+    private Queue<IEnumerator> QueuedMoves;
+    private Coroutine ActiveMovementRoutine;
+
+    public void Update()
+    {
+        if(ActiveMovementRoutine == null && QueuedMoves != null && QueuedMoves.Count != 0)
+        {
+            ActiveMovementRoutine = StartCoroutine(QueuedMoves.Dequeue());
+        }
+    }
 
     public override void TakeTurn()
     {
@@ -47,9 +69,9 @@ public class TrapEnemy : Enemy, IUnstackable
             {
                 for (int i = 0; i < AreasOfInterest.Length; i++)
                 {
-                    foreach(Vector2Int tile in AreasOfInterest[i])
+                    foreach (Vector2Int tile in AreasOfInterest[i].coords)
                     {
-                        if(Mathf.RoundToInt(player.transform.position.x) == tile.x && Mathf.RoundToInt(player.transform.position.y) == tile.y)
+                        if (Mathf.RoundToInt(player.transform.position.x) == tile.x && Mathf.RoundToInt(player.transform.position.y) == tile.y)
                         {
                             populations[i]++;
                         }
@@ -60,24 +82,130 @@ public class TrapEnemy : Enemy, IUnstackable
             {
                 if (Mathf.Max(populations) == populations[i])
                 {
-                    for (int pre = 0; pre < i; pre++)
+                    bool duplicate = false;
+                    // check to see if there are multiple max candidates and pick the current one if so
+                    for (int remaining = i + 1; remaining < populations.Length; remaining++)
                     {
-                        if(populations[i] == populations[pre])
+                        if (populations[i] == populations[remaining])
                         {
-                            // calc distance and go to closer one
+                            duplicate = true;
+
+                            /*
+                            // useless code - oops
+                            
+                            Vector2Int currentPos = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+
+                            int xdiffi = Mathf.Abs(currentPos.x - Targets[i].x);
+                            int xdiffremaining = Mathf.Abs(currentPos.x - Targets[remaining].x);
+                            int ydiffi = Mathf.Abs(currentPos.y - Targets[i].y);
+                            int ydiffremaining = Mathf.Abs(currentPos.y - Targets[remaining].y);
+                            int diffi = xdiffi + ydiffi;
+                            int diffremaining = xdiffremaining + ydiffremaining;
+
+                            if (diffi >= diffremaining)
+                            {
+                                ChosenIndex = i;
+                            }
+                            else
+                            {
+                                ChosenIndex = remaining;
+                            }
+                            */
+
                         }
                     }
-                    for (int post = i+1; post < populations.Length; post++)
+                    // otherwise pick one with uncontested max
+                    if (!duplicate)
                     {
-                        if (populations[i] == populations[post])
-                        {
-                            // calc distance and go to closer one
-                        }
+                        ChosenIndex = i;
                     }
                 }
             }
+            if (Targets[ChosenIndex] == new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y)))
+            {
+                Traps[ChosenIndex].Activate();
+                MovesRemaining = 0;
+            }
+            else
+            {
+                // deactivate all traps
+                foreach (Trap trap in Traps)
+                {
+                    trap.Deactivate();
+                }
+
+                // move toward chosen target
+                Vector2Int currentPos = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+                QueuedMoves = new Queue<IEnumerator>();
+                while (MovesRemaining > 0)
+                {
+                    if (MovesRemaining > 0 && currentPos.x > Targets[ChosenIndex].x && CheckIfValidMove(currentPos.x - 1, currentPos.y))
+                    {
+                        // move left
+                        QueuedMoves.Enqueue(Lerp(new Vector3(currentPos.x - 1, currentPos.y, 0), 0.5f));
+                        currentPos.x--;
+                        
+                    }
+                    else if (MovesRemaining > 0 && currentPos.x < Targets[ChosenIndex].x && CheckIfValidMove(currentPos.x + 1, currentPos.y))
+                    {
+                        // move right
+                        QueuedMoves.Enqueue(Lerp(new Vector3(currentPos.x + 1, currentPos.y, 0), 0.5f));
+                        currentPos.x++;
+                        
+                    }
+                    else if (MovesRemaining > 0 && currentPos.y > Targets[ChosenIndex].y && CheckIfValidMove(currentPos.x, currentPos.y - 1))
+                    {
+                        // move down
+                        QueuedMoves.Enqueue(Lerp(new Vector3(currentPos.x, currentPos.y - 1, 0), 0.5f));
+                        currentPos.y--;
+                        
+                    }
+                    else if (MovesRemaining > 0 && currentPos.y < Targets[ChosenIndex].y && CheckIfValidMove(currentPos.x, currentPos.y + 1))
+                    {
+                        // move up
+                        QueuedMoves.Enqueue(Lerp(new Vector3(currentPos.x, currentPos.y + 1, 0), 0.5f));
+                        currentPos.y++;
+                        
+                    }
+                    Debug.Log("moving");
+                    MovesRemaining--;
+                }
+            }
+            yield return null;
         }
-        yield return null;
+
+        IEnumerator Lerp(Vector3 target, float duration)
+        {
+            Vector3 start = transform.position;
+            float elapsedTime = 0f;
+
+            // playing walk animations
+            if (Ani != null)
+            {
+                if (target.x > start.x)
+                {
+                    Ani.Play(AnimationClipPrefix + "WalkRight");
+                }
+                else
+                {
+                    Ani.Play(AnimationClipPrefix + "WalkLeft");
+                }
+            }
+
+            while (elapsedTime < duration)
+            {
+                transform.position = Vector3.Lerp(start, target, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            // returning to the idle state
+            if (Ani != null)
+            {
+                Ani.Play(AnimationClipPrefix + "Idle");
+            }
+            ActiveMovementRoutine = null;
+        }
     }
 }
 
